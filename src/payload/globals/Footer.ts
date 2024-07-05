368d0917aaeca151c689969e32053c1b0206cc6f
Saeed Saeed <saeedgamebwoy@gmail.com>
2024-07-05 22:32:18 +0900

    implement ecommerce functionality

--- src/payload/globals/Footer.ts
+++ src/payload/globals/Footer.ts
@@ -9,6 +9,13 @@ export const Footer: GlobalConfig = {
   },
   fields: [
     {
+      name: 'copyright',
+      label:"copyright",
+      type:'text',
+      required:true,
+
+    },
+    {
       name: 'navItems',
       type: 'array',
       maxRows: 6,
